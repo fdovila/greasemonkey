58371023b1318c04f8eff53921550fabaea08c47
Christian Angermann <angermann@ubilabs.net>
2010-11-10 10:53:49 +0100

    added a scribble of an progress bar for basecamp extension

--- basecamp.user.js
+++ basecamp.user.js
@@ -31,7 +31,8 @@ for (var i = elems.length-1; i >= 0; i--) {
   parent.appendChild(box);
 
   // show todo id
-  box.innerHTML = '<span class="gm_task"><strong>Task #</strong>' + id.substr(id.search(/[0-9]/), id.length) + '</span>';
+  box.innerHTML = '<span class="gm_task"><strong>Task #</strong>' + id.substr(id.search(/[0-9]/), id.length) + '</span>' + 
+    '<span class="gm_progress"><span></span></span>';
 
   // highlighting
   if (/in progress/.test(string)) {
@@ -44,11 +45,13 @@ for (var i = elems.length-1; i >= 0; i--) {
 }
 
 var styles = '.gm_box{position:absolute;display:none;background-color:rgba(255,255,255,.9);border:1px solid #AAA;left:-3px;top:-3px;padding:18px 0 0;width:100%;z-zndex:-1;-moz-border-radius:3px;-webkit-border-radius:3px;-moz-box-shadow:0 2px 5px rgba(0,0,0,.5);-webkit-box-shadow:0 2px 5px rgba(0,0,0,.5);z-index:-1;}';
-styles += '[data-gm-owner]{position:relative;z-index:0;padding:0 .5em 1px .5em;background-image:-moz-linear-gradient(left,#d3e9d2 90%,#eee);}';
+styles += '[data-gm-owner]{position:relative;z-index:0;padding:0 .5em;background-image:-moz-linear-gradient(left,#d3e9d2 90%,#eee);}';
 styles += '.gm_box .gm_task{font-size:10px;color:#666;marginLeft:5px;padding:2px 4px 1px;}';
-styles += '[data-gm-owner=true]{background-color:#A8CFA8;color:#333;background-image:-moz-linear-gradient(left,#A8CFA8 90%,#eee);}';
+styles += '[data-gm-owner=true]{background-color:#A8CFA8!important;color:#333;background-image:-moz-linear-gradient(left,#A8CFA8 90%,#eee)!important;}';
 styles += '[data-gm-owner=false]{background-color:#D2E9D2}';
 styles += '.item_wrapper{z-index:0;}';
+styles += '.gm_progress{border: medium none #EEE;display: inline-block;height: 2px;line-height: 0;overflow: hidden;}';
+styles += '.gm_progress span{border-left:30px solid #090; border-right: 70px solid #fc0}';
 styles += '.item_wrapper:hover{z-index:1;}';
 styles += '[id^=item_]:hover .gm_box{display:block;}';
 styles += 'body.todos div.list a.pill_todo_item span.content{background-image:none;}';
