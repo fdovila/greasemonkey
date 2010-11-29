1cdccf1e0f744d311f95f60328bdc7f2d96babd1
Christian Angermann <angermann@ubilabs.net>
2010-11-29 18:09:20 +0100

    Added authorization for http request to basecamp

--- basecamp.user.js
+++ basecamp.user.js
@@ -19,13 +19,16 @@ var dom =  function (selector) {
 
 var get_data = function () {
   return {
-    prefix: localStorage.getItem('gm_indicator') || '',
     key: localStorage.getItem('gm_key') || ''
   };
 };
 
+var AUTH_KEY = '';
+
+var Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(c){var a="";var k,h,f,j,g,e,d;var b=0;c=Base64._utf8_encode(c);while(b<c.length){k=c.charCodeAt(b++);h=c.charCodeAt(b++);f=c.charCodeAt(b++);j=k>>2;g=((k&3)<<4)|(h>>4);e=((h&15)<<2)|(f>>6);d=f&63;if(isNaN(h)){e=d=64}else{if(isNaN(f)){d=64}}a=a+this._keyStr.charAt(j)+this._keyStr.charAt(g)+this._keyStr.charAt(e)+this._keyStr.charAt(d)}return a},decode:function(c){var a="";var k,h,f;var j,g,e,d;var b=0;c=c.replace(/[^A-Za-z0-9\+\/\=]/g,"");while(b<c.length){j=this._keyStr.indexOf(c.charAt(b++));g=this._keyStr.indexOf(c.charAt(b++));e=this._keyStr.indexOf(c.charAt(b++));d=this._keyStr.indexOf(c.charAt(b++));k=(j<<2)|(g>>4);h=((g&15)<<4)|(e>>2);f=((e&3)<<6)|d;a=a+String.fromCharCode(k);if(e!=64){a=a+String.fromCharCode(h)}if(d!=64){a=a+String.fromCharCode(f)}}a=Base64._utf8_decode(a);return a},_utf8_encode:function(b){b=b.replace(/\r\n/g,"\n");var a="";for(var e=0;e<b.length;e++){var d=b.charCodeAt(e);if(d<128){a+=String.fromCharCode(d)}else{if((d>127)&&(d<2048)){a+=String.fromCharCode((d>>6)|192);a+=String.fromCharCode((d&63)|128)}else{a+=String.fromCharCode((d>>12)|224);a+=String.fromCharCode(((d>>6)&63)|128);a+=String.fromCharCode((d&63)|128)}}}return a},_utf8_decode:function(a){var b="";var d=0;var e=c1=c2=0;while(d<a.length){e=a.charCodeAt(d);if(e<128){b+=String.fromCharCode(e);d++}else{if((e>191)&&(e<224)){c2=a.charCodeAt(d+1);b+=String.fromCharCode(((e&31)<<6)|(c2&63));d+=2}else{c2=a.charCodeAt(d+1);c3=a.charCodeAt(d+2);b+=String.fromCharCode(((e&15)<<12)|((c2&63)<<6)|(c3&63));d+=3}}}return b}};
 
 var elems = dom('.items_wrapper > div > div[id^=item_]');
+
 var init = function () {
   for (var i = elems.length-1; i >= 0; i--) {
     var elem = elems[i],
@@ -55,7 +58,7 @@ var init = function () {
           var url = loc.protocol + '/todo_items/' + id + '/time_entries.xml';  
          
           // if (localStorage.getItem('spent_time_' + id) == null) {
-            get_request(url, id, function (xhr) {
+            http_request(url, function (xhr) {
               var hours = xhr.responseXML.getElementsByTagName('hours'),
                 spent_time = 0;
               
@@ -128,41 +131,30 @@ var update_progress_bar = function (id) {
   time.innerHTML = 'Spent: ' + roundNumber(spent_time, 2) + 'h / Estimated: ' + roundNumber(estimate_time, 2) + 'h';
 };
 
+var http_request = function (url, callback) {
+  var data = get_data();
 
-var get_request = function (url, id, callback) {
-  var data = {} ;
-  data.key = '';
+  if(AUTH_KEY == ''){
+    AUTH_KEY = Base64.encode(data.key + ":x");
+  }
+  
   GM_xmlhttpRequest({
     method: "GET",
     url: url,
-    data: 'username=' + data.key + '&password=x',
     headers: {
-      "User-Agent": "Mozilla/5.0",
-      "Accept": "text/xml"
+      'User-agent': 'Mozilla/4.0 (compatible) Greasemonkey/0.3',
+      'Accept': 'application/atom+xml,application/xml,text/xml',
+      "Authorization": "Basic " + AUTH_KEY
     },
     onload: function (response) {
       if (!response.responseXML) {
         response.responseXML = new DOMParser()
           .parseFromString(response.responseText, "text/xml");
       }
-/*
-      GM_log([
-        response.status,
-        response.statusText,
-        response.readyState,
-        response.responseHeaders,
-        response.responseText,
-        response.finalUrl,
-        response.responseXML
-      ].join("\n"));
-*/
-
       callback.call(this, response);
     }
   });
 };
-
-
 
 
 var styles = '.gm_box{position:absolute;display:none;background-color:rgba(255,255,255,.9);border:1px solid #AAA;left:-3px;top:4px;padding:18px 0 0;width:100%;z-zndex:0;-moz-border-radius:3px;-webkit-border-radius:3px;-moz-box-shadow:0 2px 5px rgba(0,0,0,.5);-webkit-box-shadow:0 2px 5px rgba(0,0,0,.5);z-index:-1;}';
@@ -182,45 +174,45 @@ styles += '.item_wrapper:hover{z-index:1;}';
 styles += '[id^=item_]:hover .gm_box{display:block;}';
 styles += 'body.todos div.list a.pill_todo_item,body.todos div.list a.pill_todo_item span.content{background-image:none;}';
 styles += 'table.layout td.left{width:85%}';
-
-
 GM_addStyle(styles);
-// "hello {a:3.5, s:3} diu".replace(/(\{.*\})/, '$1')
 
 // --------------------------
 var panel = document.createElement('div');
 panel.id = 'gm_panel';
 panel.innerHTML = '' +
   '<div class="form">' + 
+    '<h2>greasmonkey helper</h2>' + 
     '<div>' + 
-      '<label for="gm_key">API token</label>' +
-      '<input id="gm_key" />' +
-    '</div>' + 
-    '<div>' + 
-      '<label for="gm_story">Story key</label>' +
-      '<input id="gm_story" />' +
-      '<span>Key string to highlight the list entry</span>' +
-    '</div>' +
-  '</div>' +   
-  '<div class="action">' + 
-    '<button type="submit">submit</button>' +
+      '<div>' + 
+        '<label for="gm_key">API token</label>' +
+        '<input id="gm_key" value="" />' +
+      '</div>' + 
+    '</div>' +   
+    '<div class="action">' + 
+      '<button type="submit">submit</button>' +
+    '</div>'
   '</div>'
 ;
 
-//document.body.appendChild(panel);
+document.body.appendChild(panel);
 var style =  '#gm_panel{-moz-box-shadow: 1px 1px 0 rgba(0, 0, 0, 0.2) inset, -1px -1px 0 rgba(200,200,200,.2) inset;-moz-border-radius: 5px;-webkit-border-radius: 5px;border-radius: 5px;color:#fff;background-color: rgba(200, 200, 200, 0.4);font-size: 10px;padding: 3px 1em;position: absolute;right: 20%;text-align: left;top: 4px;}';
 style += '#gm_panel label{display: inline-block;padding-right: 10px;}';
 style += '#gm_panel input{padding:0}';
 style += '#gm_panel button{}';
-style += '#gm_panel .form{float: left;}';
+style += '#gm_panel h2{margin-top: 0;}';
 style += '#gm_panel .form div{margin-bottom:3px}';
 style += '#gm_panel .form span{color: rgba(255, 255, 255, 0.5);display: block; margin-top:3px}';
 style += '#gm_panel .action{padding:0;border:none;margin-left:1em;float: right;}';
-//GM_addStyle(style);
+GM_addStyle(style);
 
-//dom('#gm_panel button')[0].addEventListener('click', function(){
-  
-//}, false);
+var key_elem = document.querySelector('#gm_key');
+if (localStorage.getItem(key_elem.id)) {
+  key_elem.value = localStorage.getItem(key_elem.id);
+}
 
+dom('#gm_panel button')[0].addEventListener('click', function(){
+  localStorage.setItem(key_elem.id, key_elem.value);
+  window.location = window.location.href;
+}, false);
 
 init();
